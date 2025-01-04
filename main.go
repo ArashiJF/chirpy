a5b74e9f839da0cc45208e2df148e4ce048d4129
Arashi <13753619+ArashiJF@users.noreply.github.com>
2025-01-03 22:44:06 -0500

    - Created structs to hold file server hits - created middleware to increment file server hits - created handler for metrics endpoint - created handler for reset endpoint

--- main.go
+++ main.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // increment the counter here
+		next.ServeHTTP(w, r)      // call the next handler
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func healthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "OK")
+	io.WriteString(w, http.StatusText(http.StatusOK))
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	var apiCfg = apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.HandleFunc("/reset", apiCfg.reset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
